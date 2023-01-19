acec64358a9f6fe6aec5b501772e16a28f3ba238
BeeMOre32 <windowssart01@gmail.com>
2023-01-19 17:40:29 +0900

    feat: day 18 BackgroundSlider

---
+++ choehawnseok/day 18 img slider/script.js
@@ -0,0 +1,38 @@
+const body = document.querySelector("body");
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.querySelector("#left");
+const rightBtn = document.querySelector("#right");
+
+let activeSlide = 0;
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+setBgToBody();
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[activeSlide].classList.add("active");
+}
