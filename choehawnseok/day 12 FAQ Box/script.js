f83d932c37f382f33a331e988fb7848ad1fdd848
janghwahyun <1117jhh@gmail.com>
2023-01-27 23:20:07 +0900

    [20230127] Day 7,8

--- choehawnseok/day 12 FAQ Box/script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelectorAll(".faq-toggle");
-
-toggleBtn.forEach((value) => {
-  value.addEventListener("click", (e) => {
-    e.currentTarget.parentNode.classList.toggle("active");
-  });
-});
