9428cba4b5ce2ed547a7350fc84b6f209e762f1f
janghwahyun <1117jhh@gmail.com>
2023-01-29 17:27:29 +0900

    [20230129] Day 9

---
+++ .history/Jang/SoundBoard/script_20230129171243.js
@@ -0,0 +1,12 @@
+const sounds =['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+const btn = document.createElement('button')
+btn.classList.add('btn')
+btn.innerText= sound
+btn.addEventListener(()=>{
+  document.getElementById(sound).onplay() 
+})
+
+document.getElementById('buttons').appendChild(btn)
+})
---
+++ .history/Jang/SoundBoard/script_20230129171255.js
@@ -0,0 +1,11 @@
+const sounds =['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+const btn = document.createElement('button')
+btn.classList.add('btn')
+btn.innerText= sound
+btn.addEventListener(()=>{
+  document.getElementById(sound).play() 
+})
+document.getElementById('buttons').appendChild(btn)
+})
